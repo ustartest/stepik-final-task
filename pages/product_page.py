bc9e8b3764ce715c3b8830911d90cbb2da759163
austilko <ustilkoartem@gmail.com>
2021-03-24 06:11:38 +0300

    pytest paremetrize promo links were added and assert fixed

--- pages/product_page.py
+++ pages/product_page.py
@@ -17,14 +17,14 @@ class ProductPage(BasePage):
             "Product added to cart message not visible"
 
     def cart_message_contain_product_title(self):
-        assert self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE).text in \
+        assert self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE).text == \
                self.browser.find_element(*ProductPageLocators.CART_PRODUCT_TITLE).text, \
-            "Cart message not contain product title"
+               "Cart message not contain product title"
 
     def cart_price_message_exist(self):
         assert BasePage.is_element_present(self, *ProductPageLocators.CART_PRICE_MESSAGE), \
             "Cart price message not visible"
 
     def cart_price_contain_product_price(self):
-        assert self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text in \
+        assert self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text == \
                self.browser.find_element(*ProductPageLocators.CART_PRICE).text, "Cart price not contain product price"
